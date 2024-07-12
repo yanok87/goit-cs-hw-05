a6a48f36bf6d8c432bfdab5a5ee0078e9126a8b2
Yana <yanok87@users.noreply.github.com>
2024-07-12 21:05:49 +0300

    replace mapReduce

--- task_2/main.py
+++ task_2/main.py
@@ -1,24 +1,45 @@
 import requests
-import collections
-import concurrent.futures
+from collections import defaultdict
 import matplotlib.pyplot as plt
-import re
-from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+import concurrent.futures
 
 
+# Функції MapReduce
 def map_function(text):
-    words = re.findall(r"\b\w+\b", text.lower())
-    return collections.Counter(words)
+    words = text.split()
+    return [(word, 1) for word in words]
 
 
-def reduce_function(counters):
-    total_counter = collections.Counter()
-    for counter in counters:
-        total_counter.update(counter)
-    return total_counter
+def shuffle_function(mapped_values):
+    shuffled = defaultdict(list)
+    for key, value in mapped_values:
+        shuffled[key].append(value)
+    return shuffled.items()
 
 
+def reduce_function(shuffled_values):
+    reduced = {}
+    for key, values in shuffled_values:
+        reduced[key] = sum(values)
+    return reduced
+
+
+# Виконання MapReduce
+def map_reduce(text):
+    # Крок 1: Мапінг
+    mapped_values = map_function(text)
+
+    # Крок 2: Shuffle
+    shuffled_values = shuffle_function(mapped_values)
+
+    # Крок 3: Редукція
+    reduced_values = reduce_function(shuffled_values)
+
+    return reduced_values
+
+
+# Функція для завантаження тексту з URL
 def fetch_text_from_url(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -27,8 +48,12 @@ def fetch_text_from_url(url):
     return soup.get_text()
 
 
+# Функція для візуалізації результатів
 def visualize_top_words(word_counts, num_top_words=10):
-    top_words = word_counts.most_common(num_top_words)
+    sorted_word_counts = sorted(
+        word_counts.items(), key=lambda item: item[1], reverse=True
+    )
+    top_words = sorted_word_counts[:num_top_words]
     words, counts = zip(*top_words)
 
     plt.figure(figsize=(10, 6))
@@ -40,16 +65,22 @@ def visualize_top_words(word_counts, num_top_words=10):
     plt.show()
 
 
+# Головний блок коду
 def main(url):
     try:
         text = fetch_text_from_url(url)
         text_blocks = [text[i : i + 10000] for i in range(0, len(text), 10000)]
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            map_results = list(executor.map(map_function, text_blocks))
+            map_reduce_results = list(executor.map(map_reduce, text_blocks))
 
-        word_counts = reduce_function(map_results)
-        visualize_top_words(word_counts)
+        # Об'єднання результатів з усіх блоків
+        combined_results = defaultdict(int)
+        for result in map_reduce_results:
+            for word, count in result.items():
+                combined_results[word] += count
+
+        visualize_top_words(combined_results)
     except Exception as e:
         print(f"An error occurred: {e}")
 
